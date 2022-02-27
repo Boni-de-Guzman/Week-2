db3b1b0b3d930cd538bf50943d45ca6d445c31e3
Boni De Guzman <99787855+Boni-de-Guzman@users.noreply.github.com>
2022-02-26 18:16:51 -0800

    Update password.js

--- password.js
+++ password.js
@@ -6,19 +6,17 @@ const reader = readline.createInterface({
 });
 
 
-reader.question("Hello please create your password!", function(input){
-	tokens = input.split(' ');
-	
-	text = tokens[0];
+reader.question("Hello please create your password (minimum of 10 characters)", function(input){
+   const firstText = "Password requirement did not meet. You only put"
+   tokens = input.split(' ');
+   text = tokens[0];
 
 
-	if (text.length <= 10 ){
-		console.log("Password requirement did not meet");
+	if (text.length <= 9 ){
+		console.log(`${firstText} ${input.length}`);
 	} else {
         console.log ("Password requirement met")
-
-    }
+  }
     reader.close()
 
 });
-
