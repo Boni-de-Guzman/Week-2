a9bbebf7b207871da44e6f8ccacb57d5b0ab4cbb
Boni De Guzman <99787855+Boni-de-Guzman@users.noreply.github.com>
2022-02-27 19:46:57 -0800

    Update password.js

--- password.js
+++ password.js
@@ -7,16 +7,15 @@ const reader = readline.createInterface({
 
 
 reader.question("Hello please create your password (minimum of 10 characters)", function(input){
-   const firstText = "Password requirement did not meet. You only put"
+   const firstText = "Password requirement did not meet. You only put";
    tokens = input.split(' ');
    text = tokens[0];
 
 
 	if (text.length <= 9 ){
-		console.log(`${firstText} ${input.length}`);
+	console.log(`${firstText} ${input.length}`);
 	} else {
         console.log ("Password requirement met")
-  }
-    reader.close()
-
-});
+        } 
+        reader.close()
+        });
